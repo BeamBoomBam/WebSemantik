5d11639b21f764d67afe784071c4dd518ff41714
Tobias Renal <1872015@maranatha.ac.id>
2020-10-27 12:28:27 +0700

    Web Semantik

---
+++ schema.js
@@ -0,0 +1,106 @@
+const axios = require('axios')
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull
+} = require('graphql')
+const server = 'http://localhost:3000'
+
+const filmType = new GraphQLObjectType({
+    name: 'film',
+    fields: () => ({
+      id: { type: GraphQLInt },
+      title: { type: GraphQLString },
+      director: { type: GraphQLString },
+      year: { type: GraphQLInt },
+      genre :{ type: GraphQLString },
+      score :{ type: GraphQLInt },
+      filmRate:{ type:GraphQLString }
+    }),
+  })
+
+  const AnimeType = new GraphQLObjectType({
+    name: 'Anime',
+    fields: () => ({
+      id: { type: GraphQLInt },
+      title: { type: GraphQLString },
+      director: { type: GraphQLString },
+      year: { type: GraphQLInt },
+    }),
+  })
+
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    animes: {
+      type: new GraphQLList(AnimeType),
+      resolve(_parentValue_, _args_) {
+        return axios.get(`${server}/animes/`)
+          .then(res => res.data)
+      }
+    }
+  }
+})
+
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addAnime: {
+      type: AnimeType,
+      args: {
+        title: { type: new GraphQLNonNull(GraphQLString) },
+        director: { type: new GraphQLNonNull(GraphQLString) },
+        year: { type: new GraphQLNonNull(GraphQLInt) },
+      },
+      resolve(parentValue, args) {
+        return axios.post(`${server}/animes`, {
+          title: args.title,
+          anime: args.anime,
+          year: args.year,
+        }).then(res => res.data)
+      },
+    },
+    findAnime: {
+      type: AnimeType,
+      args: {
+        id: { type: GraphQLInt },
+      },
+      resolve(parentValue, args) {
+        return axios.get(`${server}/animes/${args.id}`)
+          .then(res => res.data)
+      }
+    },
+    updateAnime: {
+      type: AnimeType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLInt) },
+        title: { type: GraphQLString },
+        director: { type: GraphQLString },
+        year: { type: GraphQLInt },
+      },
+      resolve(parentValue, args) {
+        return axios.put(`${server}/animes/${args.id}`, args)
+          .then(res => res.data)
+      },
+    },
+    deleteAnime: {
+      type: AnimeType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLInt) },
+      },
+      resolve(parentValue, args) {
+        return axios.delete(`${server}/animes/${args.id}`, args )
+         .then(res => res.data)
+      },
+    },
+  }
+})
+
+  module.exports = new GraphQLSchema({
+    query: RootQuery,
+    mutation,
+  })
